a2c481719cd106cc538c6331b4cef3dc8046c3d5
Yan Liang <70448034+yanliang12@users.noreply.github.com>
2020-10-08 22:22:51 +0400

    Update yan_yolo.py

--- yan_yolo.py
+++ yan_yolo.py
@@ -1,3 +1,4 @@
+##############yan_yolo.py##############
 """
 Retrain the YOLO model for your own dataset.
 """
@@ -113,17 +114,18 @@ def data_generator_wrapper(annotation_lines, batch_size, input_shape, anchors, n
 
 def train_yan_yolo_model(
     annotation_path = 'train.txt',
-    log_dir = 'logs/000/',
     classes_path = 'model_data/voc_classes.txt',
     anchors_path = 'model_data/yolo_anchors.txt',
     trained_weights_final_file = 'trained_weights_final.h5',
-    trained_weights_stage_1 = 'trained_weights_stage_1.h5'
+    trained_weights_stage_1 = 'trained_weights_stage_1.h5',
+    batch_size = 32,
+    epochs_stage_1 = 10,
+    epochs_fine_tunning = 20
     ):
-    
+
     class_names = get_classes(classes_path)
     num_classes = len(class_names)
     anchors = get_anchors(anchors_path)
-
     input_shape = (416,416) # multiple of 32, hw
 
     is_tiny_version = len(anchors)==6 # default setting
@@ -135,14 +137,17 @@ def train_yan_yolo_model(
             freeze_body=2, weights_path='model_data/yolo_weights.h5') # make sure you know what you freeze
 
     logging = TensorBoard(log_dir=log_dir)
-    checkpoint = ModelCheckpoint(log_dir + 'ep{epoch:03d}-loss{loss:.3f}-val_loss{val_loss:.3f}.h5',
-        monitor='val_loss', save_weights_only=True, save_best_only=True, period=3)
+    checkpoint = ModelCheckpoint(
+        log_dir + 'ep{epoch:03d}-loss{loss:.3f}-val_loss{val_loss:.3f}.h5',
+        save_weights_only=True, 
+        save_best_only=True, period=3)
     reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=3, verbose=1)
     early_stopping = EarlyStopping(monitor='val_loss', min_delta=0, patience=10, verbose=1)
 
     val_split = 0.1
     with open(annotation_path) as f:
         lines = f.readlines()
+
     np.random.seed(10101)
     np.random.shuffle(lines)
     np.random.seed(None)
@@ -151,39 +156,41 @@ def train_yan_yolo_model(
 
     # Train with frozen layers first, to get a stable loss.
     # Adjust num epochs to your dataset. This step is enough to obtain a not bad model.
-    if True:
-        model.compile(optimizer=Adam(lr=1e-3), loss={
-            # use custom yolo_loss Lambda layer.
-            'yolo_loss': lambda y_true, y_pred: y_pred})
+    model.compile(optimizer=Adam(lr=1e-3), loss={
+        # use custom yolo_loss Lambda layer.
+        'yolo_loss': lambda y_true, y_pred: y_pred})
 
-        batch_size = 32
-        print('Train on {} samples, val on {} samples, with batch size {}.'.format(num_train, num_val, batch_size))
-        model.fit_generator(data_generator_wrapper(lines[:num_train], batch_size, input_shape, anchors, num_classes),
-                steps_per_epoch=max(1, num_train//batch_size),
-                validation_data=data_generator_wrapper(lines[num_train:], batch_size, input_shape, anchors, num_classes),
-                validation_steps=max(1, num_val//batch_size),
-                epochs=50,
-                initial_epoch=0,
-                callbacks=[logging, checkpoint])
-        model.save_weights(log_dir + trained_weights_stage_1)
+    print('Train on {} samples, with batch size {}.'.format(num_train, batch_size))
+
+    model.fit_generator(
+        data_generator_wrapper(lines[:num_train], batch_size, input_shape, anchors, num_classes),
+        steps_per_epoch=max(1, num_train//batch_size),
+        epochs=epochs_stage_1,
+        initial_epoch=0)
+
+    if trained_weights_stage_1 is not None:
+        model.save_weights(trained_weights_stage_1)
 
     # Unfreeze and continue training, to fine-tune.
     # Train longer if the result is not good.
-    if True:
-        for i in range(len(model.layers)):
-            model.layers[i].trainable = True
-        model.compile(optimizer=Adam(lr=1e-4), loss={'yolo_loss': lambda y_true, y_pred: y_pred}) # recompile to apply the change
-        print('Unfreeze all of the layers.')
+    for i in range(len(model.layers)):
+        model.layers[i].trainable = True
 
-        batch_size = 32 # note that more GPU memory is required after unfreezing the body
-        print('Train on {} samples, val on {} samples, with batch size {}.'.format(num_train, num_val, batch_size))
-        model.fit_generator(data_generator_wrapper(lines[:num_train], batch_size, input_shape, anchors, num_classes),
-            steps_per_epoch=max(1, num_train//batch_size),
-            validation_data=data_generator_wrapper(lines[num_train:], batch_size, input_shape, anchors, num_classes),
-            validation_steps=max(1, num_val//batch_size),
-            epochs=100,
-            initial_epoch=50,
-            callbacks=[logging, checkpoint, reduce_lr, early_stopping])
-        model.save_weights(log_dir + trained_weights_final_file)
+    model.compile(
+        optimizer=Adam(lr=1e-4), 
+        loss={'yolo_loss': lambda y_true, y_pred: y_pred}
+        ) # recompile to apply the change
+    print('Unfreeze all of the layers.')
+
+    # note that more GPU memory is required after unfreezing the body
+    print('Train on {} samples, with batch size {}.'.format(num_train, batch_size))
+    model.fit_generator(
+        data_generator_wrapper(lines[:num_train], batch_size, input_shape, anchors, num_classes),
+        steps_per_epoch=max(1, num_train//batch_size),
+        epochs=epochs_fine_tunning,
+        initial_epoch=epochs_stage_1
+        )
+    model.save_weights(trained_weights_final_file)
 
     # Further training if needed.
+##############yan_yolo.py##############
